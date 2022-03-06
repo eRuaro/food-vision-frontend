15f3f5e142ab6f8b8810b6841f1f8289e97bf979
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-06 09:51:40 +0800

    added factory method for parsing json

--- lib/models/food_vision_prediction.dart
+++ lib/models/food_vision_prediction.dart
@@ -4,4 +4,15 @@ class FoodVisionPrediction {
 
   final String classPrediction;
   final String confidenceScore;
+
+  factory FoodVisionPrediction.fromJson(Map<String, dynamic> data) {
+    final String classPrediction = data["model-prediction"] as String;
+    final String confidenceScore =
+        data["model-prediction-confidence-score"] as String;
+
+    return FoodVisionPrediction(
+      classPrediction: classPrediction,
+      confidenceScore: confidenceScore,
+    );
+  }
 }
