1622694cf2412ed381fede6a01d8dd9a92a3f7ce
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-06 16:00:15 +0800

    added cubit method for geting image prediction

--- lib/cubits/food_vision_prediction_cubit.dart
+++ lib/cubits/food_vision_prediction_cubit.dart
@@ -1,8 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_vision_frontend/models/food_vision_prediction.dart';
+import 'package:food_vision_frontend/services/image_prediction.dart';
 
 part 'food_vision_prediction_state.dart';
 
 class FoodVisionPredictionCubit extends Cubit<FoodVisionPredictionState> {
-  FoodVisionPredictionCubit() : super(FoodVisionPredictionInitial());
+  FoodVisionPredictionCubit() : super(const FoodVisionPredictionInitial());
+
+  void getImagePrediction(String url) async {
+    try {
+      emit(const FoodVisionPredictionLoading());
+      FoodVisionPrediction prediction =
+          await ImagePrediction().getImagePrediction(url);
+      emit(
+        FoodVisionPredictionLoaded(
+          prediction: prediction.classPrediction,
+          confidenceScore: prediction.confidenceScore,
+        ),
+      );
+    } catch (e) {
+      emit(
+        FoodVisionPredictionError(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
 }
