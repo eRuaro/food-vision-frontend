9448db59013314df052bddcbaaa0ee211d7c7c88
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-06 16:17:10 +0800

    deleted windows build

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:food_vision_frontend/services/image_prediction.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    const String url =
-        "https://tallypress.com/wp-content/uploads/2017/11/7-irresistible-chocolate-cakes-you-should-try-in-klang-valley.jpg";
-    ImagePrediction().getImagePrediction(url);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Food Vision: Predicts Food Classification based on Image URL",
-        ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: Center(
+              child: Text(
+                'Food Vision Front-End',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ]        
       ),
-      body: Container(),
     );
   }
 }
