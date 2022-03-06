42e44f71f6d5bee1bf1591033f41c6b9cb2a2e54
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-06 09:47:24 +0800

    created model for prediction

---
+++ lib/models/food_vision_prediction.dart
@@ -0,0 +1,7 @@
+class FoodVisionPrediction {
+  const FoodVisionPrediction(
+      {required this.classPrediction, required this.confidenceScore});
+
+  final String classPrediction;
+  final String confidenceScore;
+}
