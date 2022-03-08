9f7eb54a05a5a4bddfe4f020fe656b4c03dd114e
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-08 08:56:58 +0800

    added color palette

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_vision_frontend/models/color_palette.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,18 +8,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorPalette.lavanderWeb,
       body: Column(
         children: [
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.2, 
           ),
-          const Align(
+          Align(
             alignment: Alignment.topCenter,
             child: Text(
               'Food Vision Front-End',
-              style: TextStyle(
-                fontSize: 24,
+              style: GoogleFonts.spaceMono(
+                fontSize: 44,
                 fontWeight: FontWeight.bold,
+                color: ColorPalette.carribeanGreen,
               ),
             ),
           ),
